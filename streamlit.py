2a74f1f5cc8556eaca70387d67e8733b5441e611
irosenfe <israelinteldevicelink@gmail.com>
2025-05-06 10:36:17 +0300

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit.logger import get_logger
+logger = get_logger(__name__)
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv('MY_HF_API_TOKEN')
+token = os.getenv('home')
+print(token)
+st.title("Streamlit Israel")
+st.text_area("Jira MDE")
+repo_id = "microsoft/streamlit"
